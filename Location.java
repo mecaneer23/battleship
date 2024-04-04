7cc39e360e1e8fb43b66b062196a84ad1d7a83b9
mecaneer23 <mecaneer23@gmail.com>
2024-04-03 21:03:34 -0500

    refactor: remove unused functions in location

--- Location.java
+++ Location.java
@@ -12,18 +12,6 @@ public class Location {
         this.status = Status.UNGUESSED;
     }
 
-    public boolean checkHit() {
-        return this.status == Status.HIT;
-    }
-
-    public boolean checkMiss() {
-        return this.status == Status.MISSED;
-    }
-
-    public boolean isUnguessed() {
-        return this.status == Status.UNGUESSED;
-    }
-
     public void markHit() {
         this.status = Status.HIT;
     }
@@ -38,10 +26,6 @@ public class Location {
 
     public void setShip(boolean val) {
         this.ship = val;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
     }
 
     public Status getStatus() {
